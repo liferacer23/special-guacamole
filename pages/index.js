d51403a91a763c9bc5da002dcc611af88172b4c8
Binyam Getachew <binyamg23@gmail.com>
2022-09-22 23:06:11 +0300

    working progress deliver section

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import styled from "styled-components";
 import Button from "antd/lib/button";
 import Image from "next/image";
 import volva from "../assets/vol.jpg";
+import checklistbg from "../assets/checklistbg.png";
 import generator from "../assets/generatorindex.jpeg";
 const HeaderContainer = styled.div``;
 const VolvaContainer = styled.div`
@@ -21,22 +22,45 @@ const AboutUsContainer = styled.div`
   padding-left: 4rem;
   padding-top: 4rem;
 `;
-const AboutUsDescription = styled.div`
+const Description = styled.div`
   display: flex;
   align-item: center;
   justify-content: start;
   flex-direction: column;
   width: 50%;
 `;
-const AboutUsText = styled.h1`
-font-size:${(props) => props.fontSize || "14px"}};
-`;
-const Section = styled.div
 const Text = styled.h1`
-  font-size: ${(props) => props.fontSize || "15px"};
-  color: ${(props) => props.color || "#000000"};
-  font-weight: ${(props) => props.fontWeight || "#000000"};
+font-size:${(props) => props.fontSize || "14px"}};
+color: ${(props) => props.color || "#000000"};
+font-weight: ${(props) => props.fontWeight || "#000000"};
 `;
+const DeliverSection = styled.div`
+  height: 30rem;
+  width: 100%;
+  background: blue;
+  position: relative;
+  display: flex;
+  align-item: center;
+  justify-content: start;
+`;
+const DeliverSectionDescription = styled.div`
+  display: flex;
+  align-item: center;
+  justify-content: start;
+  flex-direction: column;
+  width: 50%;
+  height: 100%;
+  padding: 6rem;
+`;
+
+const DeliverSectionImage = styled.div`
+  display: flex;
+  align-item: center;
+  justify-content: end;
+  width: 50%;
+  height: 100%;
+`;
+
 export default function Home() {
   return (
     <>
@@ -69,12 +93,10 @@ export default function Home() {
 
       <AboutUsContainer>
         <Image src={generator} width={500} height={600} alt="generator" />
-        <AboutUsDescription>
-          <AboutUsText fontSize="14px">About Us</AboutUsText>
-          <AboutUsText fontSize="30px">
-            We Have Everything That You Needed
-          </AboutUsText>
-          <AboutUsText fontSize="13px">
+        <Description>
+          <Text fontSize="14px">About Us</Text>
+          <Text fontSize="30px">We Have Everything That You Needed</Text>
+          <Text fontSize="13px">
             Nileco Electric Equipment Manufacturing PLC is your ultimate
             solution provider of generators, switchgears, and electrical spare
             parts.
@@ -87,9 +109,43 @@ export default function Home() {
             Aenean tincidunt id mauris id auctor. an countries during the
             Industrial mercantile and feudal economies.ThDonec atnissim quis
             neque inter dum, quis porta sem ng the Industria
-          </AboutUsText>
-        </AboutUsDescription>
+          </Text>
+        </Description>
       </AboutUsContainer>
+      <DeliverSection>
+        <DeliverSectionDescription>
+          {" "}
+          <Text fontSize="14px" color="#fff">
+            WE DELIVER ON TIME ,ALL THE TIME
+          </Text>
+          <Text fontSize="30px" color="#fff">
+            Great Experience For Building Construction & Outdoor Projects
+          </Text>
+          <Text fontSize="13px" color="#fff">
+            The Easiest Way To Get What Need Need It Fast? We Can Help. We work
+            to reduce air emissions industriel has a brilliant Capitalise on low
+            hanging fruit to identify You get speed, flexibility and better
+            control F
+            <br />
+            <br />
+            Committed to delivering satisfaction and competence, we at Nileco
+            strive to look after our clients’ energy needs.
+            <br />
+            <br />
+            Aenean tincidunt id mauris id auctor. an countries during the
+            Industrial mercantile and feudal economies.ThDonec atnissim quis
+            neque inter dum, quis porta sem ng the Industria
+          </Text>
+        </DeliverSectionDescription>
+        <DeliverSectionImage>
+          <Image
+            style={{ opacity: 0.5 }}
+            src={checklistbg}
+            width={500}
+            height={100}
+          />
+        </DeliverSectionImage>
+      </DeliverSection>
     </>
   );
 }

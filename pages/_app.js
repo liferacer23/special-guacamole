39f327cb1516f455e839deaf9b3f0b8f2a7feca7
Binyam Getachew <binyamg23@gmail.com>
2022-10-23 12:08:09 +0300

    lighthouse

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 
 import "./../styles/globals.css";
 import Layout from "../components/Layout/Layout";
-export const config = {unstable_runtimeJS: false}
+
 function MyApp({ Component, pageProps }) {
   return (
     <>

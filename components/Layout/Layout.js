7fc4e866a0a4bfa79b6158fcb29743c9692aa3fd
Binyam Getachew <binyamg23@gmail.com>
2022-11-06 14:40:31 +0300

    minor fixes and changes

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -3,9 +3,16 @@ import NavBar from "../navbar";
 import Footer from "../Footer/";
 export default function Layout({ children }) {
   return (
-    <div style={{position:'relative', minHeight:'100vh'}}>
-      <NavBar priority/>
-      <div style={{zIndex:"1000"}}>{children}</div>
+    <div
+      style={{
+        position: "relative",
+        width: "100vw",
+        height: "100vh",
+        overflowX: "clip",
+      }}
+    >
+      <NavBar priority />
+      <div style={{ zIndex: "1000" }}>{children}</div>
       <Footer />
     </div>
   );

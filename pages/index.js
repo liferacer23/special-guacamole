1d8cad1ba74d401adfd79371c22fcde5ff073db3
Binyam Getachew <binyamg23@gmail.com>
2022-11-06 15:01:51 +0300

    minor fixes and changes

--- pages/index.js
+++ pages/index.js
@@ -1170,7 +1170,7 @@ export default function Home() {
                   mobileFontSize="0.6rem"
                   width="15rem"
                   height="1rem"
-                  mobileWidth="8rem"
+                  mobileWidth="9rem"
                 >
                   Call Us at +251977805757
                 </Text>

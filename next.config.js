4093c1e7eef0d9c4bac57067a8cefbc6170acc3a
Binyam Getachew <binyamg23@gmail.com>
2022-11-07 21:10:57 +0300

    Header getting cut fix and table border added

--- next.config.js
+++ next.config.js
@@ -1,41 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  reactStrictMode: false,
+  swcMinify: false,
 };
-const BundleAnalyzerPlugin =
-  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
-module.exports = {
-  plugins: [new BundleAnalyzerPlugin()],
-};
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-module.exports = withBundleAnalyzer({});
 module.exports = nextConfig;
-module.exports =
-  ({ enabled = true, openAnalyzer = true } = {}) =>
-  (nextConfig = {}) => {
-    return Object.assign({}, nextConfig, {
-      webpack(config, options) {
-        if (enabled) {
-          const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
-          config.plugins.push(
-            new BundleAnalyzerPlugin({
-              analyzerMode: "static",
-              openAnalyzer,
-              reportFilename: options.isServer
-                ? "../analyze/server.html"
-                : "./analyze/client.html",
-            })
-          );
-        }
-
-        if (typeof nextConfig.webpack === "function") {
-          return nextConfig.webpack(config, options);
-        }
-        return config;
-      },
-    });
-  };
